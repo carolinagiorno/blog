d93deaff1300c4265bb39328f1799b97e43e790d
carolinagiorno <chagiorno@gmail.com>
2020-02-18 16:43:09 -0300

    Ajustes iniciais

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+	<Layout>
+	    <SEO title = "404: Not found"/>
+	    <h1> 404 </h1>
+    <p>Ops! Essa página não existe.</p>
+    </Layout>
 )
 
 export default NotFoundPage
