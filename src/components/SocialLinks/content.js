c1534dda49f148670e80faf46cbcfefe9241472a
carolinagiorno <chagiorno@gmail.com>
2020-02-20 21:24:16 -0300

    Add sidebar

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,30 @@
+const handle = 'carolinagiorno'
+
+const links = [
+  {
+    label: 'Github',
+    url: `https://github.com/${handle}`
+  },
+  {
+    label: 'Twitter',
+    url: `https://twitter.com/${handle}`
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/carolina-giorno-186077191/'
+  },
+  {
+    label: 'DevTo',
+    url: `https://dev.to/${handle}`
+  },
+  {
+    label: 'Mastodon',
+    url: `https://mastodon.social/@${handle}`
+  },
+  {
+    label: 'Instagram',
+    url: `https://instagram.com/${handle}`
+  }
+]
+
+export default links
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,17 @@
+import { Github } from 'styled-icons/boxicons-logos/Github'
+import { Twitter } from 'styled-icons/boxicons-logos/Twitter'
+import { Linkedin } from 'styled-icons/boxicons-logos/Linkedin'
+import { DevTo } from 'styled-icons/boxicons-logos/DevTo'
+import { Instagram } from 'styled-icons/boxicons-logos/Instagram'
+import { Mastodon } from 'styled-icons/remix-fill/Mastodon'
+
+const Icons = {
+  Github,
+  Twitter,
+  Linkedin,
+  DevTo,
+  Mastodon,
+  Instagram
+}
+
+export default Icons
